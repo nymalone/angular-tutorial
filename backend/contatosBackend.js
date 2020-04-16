c5c10202677ca713991751cdaf6cee76ab1b05a8
Nykolle Malone <nykolle.malone@hotmail.com>
2020-04-16 09:45:07 -0300

    backend in progress

--- backend/contatosBackend.js
+++ backend/contatosBackend.js
@@ -1,15 +1,15 @@
-/* var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
 
-var contatos = [
+let contatos = [
 	{nome: "Bruno", telefone: "9999-2222", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}},
 	{nome: "Sandra", telefone: "9999-3333", data: new Date(), operadora: {nome: "Vivo", codigo: 15, categoria: "Celular"}},
 	{nome: "Mariana", telefone: "9999-9999", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}}
 ];
-var operadoras = [
+let operadoras = [
 	{nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
 	{nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1},
 	{nome: "Tim", codigo: 41, categoria: "Celular", preco: 3},
@@ -17,7 +17,7 @@ var operadoras = [
 	{nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 2}
 ];
 
-app.listen(process.env.PORT || 3412);
+app.listen(process.env.PORT || 5500);
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -37,4 +37,4 @@ app.post('/contatos', function(req, res) {
 
 app.get('/operadoras', function(req, res) {
   res.json(operadoras);
-}); */
+});
